884178724cea77e5a3d8a31c968579cf4b789e9b
adanjabbar <adanjabbar1352@gmail.com>
2025-11-16 19:54:18 +0500

    Create Lab Task 3.java

---
+++ Lab Task 3/Lab Task 3.java
@@ -0,0 +1,14 @@
+Consider the following code segment. Determine the value of b3 boolean b1=true; boolean b2=false; boolean b3=(b1==b2);
+
+public class L3{
+ public static void main(String[]args){
+
+boolean b1=true;
+boolean b2=false;
+boolean b3=(b1==b2);
+
+	System.out.println("if b1 = true, b2 = false, b3 = (b1==b2) ");
+
+	System.out.println("The value of b3 is: "+ b3);
+}
+} 
